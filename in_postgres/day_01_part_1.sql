d79fe7f524e4a8a4ce33f8f7efa46492708a0306
Jell <jean-louis@jawaninja.com>
2020-12-05 15:28:38 +0100

    Piff

--- in_postgres/day_01_part_1.sql
+++ in_postgres/day_01_part_1.sql
@@ -1,19 +1,19 @@
-DROP TABLE IF EXISTS dec01;
+DROP TABLE IF EXISTS day01;
 
-CREATE TABLE dec01 (
+CREATE TABLE day01 (
     line_number bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
     value bigint NOT NULL
 );
 
-\COPY dec01 (value) FROM PROGRAM 'cat ../inputs/day01.txt';
-VACUUM ANALYZE dec01;
+\COPY day01 (value) FROM PROGRAM 'cat ../inputs/day01.txt';
+VACUUM ANALYZE day01;
 
-CREATE INDEX ON dec01 (value, line_number);
+CREATE INDEX ON day01 (value, line_number);
 
 \timing on
 
 SELECT concat('Day 01 - Part 1: ', (a.value * b.value))
-from dec01 as a, dec01 as b
+from day01 as a, day01 as b
 where
     a.value <= 2020
 and b.value  = 2020 - a.value
