2253d6577eaea30f16edbb4fce7cf5084c043216
Jell <jean-louis@jawaninja.com>
2020-12-05 15:14:21 +0100

    Add Haskell

---
+++ in_haskell/README.md
@@ -0,0 +1,4 @@
+# In Haskell
+
+## day_01
+![day_01](./day_01_diff.svg)
---
+++ in_haskell/day_01_part_1.hs
@@ -0,0 +1,26 @@
+#!/usr/bin/env stack --resolver lts-14.14 runghc
+
+-- when running in ghci:
+-- :cd ~/DevPerso/advent2020/in_haskell
+
+import Control.Category
+import Data.Function
+
+parseInt :: String -> Int
+parseInt x = read $ filter (`elem` "-0123456789") x
+
+loadInput :: IO ([Int])
+loadInput = do
+  raw <- readFile "../inputs/day01.txt"
+  return $ map parseInt $ lines raw
+
+solve :: [Int] -> Int
+solve expenses = head $ [a * b | a <- expenses, b <- expenses, a + b == 2020]
+
+main :: IO ()
+main = do
+  input <- loadInput
+  putStr "Day 01 - Part 1: "
+  putStrLn $ show $ head $ [a * b | a <- input,
+                                    b <- input,
+                                    a + b == 2020]
